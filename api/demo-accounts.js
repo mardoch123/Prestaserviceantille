8a659eebe089373403c2f1179fb63b8d26e3bd38
mardoch123 <djossoumardoch@gmail.com>
2026-02-05 18:03:22 +0100

    g

--- api/demo-accounts.js
+++ api/demo-accounts.js
@@ -46,6 +46,15 @@ function normalizeRole(role) {
 
 export default async function handler(req, res) {
   try {
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+    }
+
     const adminClient = getSupabaseAdminClient();
     const user = await getUserFromAuthHeader(req);
 
